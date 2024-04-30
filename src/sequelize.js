db5129c9772474fd53a07fad8b79712a7cb3f475
anthonymeny <anthony.meny@noctua.care>
2024-04-30 11:43:41 +0200

    modifs

--- src/sequelize.js
+++ src/sequelize.js
@@ -3,6 +3,6 @@ const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize('agendaDB', 'postgres', 'postgres', {
     host: 'localhost',
     dialect: 'postgres',
-    port: 5432,
+    port: 5454,
 });
 module.exports = sequelize;
