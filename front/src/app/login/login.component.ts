79246215b8d91ec3ed803f0d16bcdbd281c1ebb9
anthonymeny <anthony.meny@noctua.care>
2024-04-30 16:25:38 +0200

    refacto

--- front/src/app/login/login.component.ts
+++ front/src/app/login/login.component.ts
@@ -1,31 +1,35 @@
-import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { BrowserModule } from '@angular/platform-browser';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  standalone: true,
-  imports: [FormsModule, HttpClientModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.sass'
+  styleUrls: ['./login.component.sass']
 })
-
 export class LoginComponent {
   login!: string;
   password!: string;
+  private apiUrl = 'http://localhost:3000/api/v1';
 
-  constructor(private router: Router, private http: HttpClient) { }
+  constructor(private router: Router) { }
 
-  loginUser(login: string, password: string) {
-    this.http.post('http://localhost:3000/login', { login: this.login, password: this.password }).subscribe((res: any) => {
-      if (res.token) {
-        localStorage.setItem('token', res.token);
-        this.router.navigate(['/home']);
-      } else {
-        alert('Incorrect login or password');
-      }
-    });
+  loginUser(login: string, password: string): void {
+    fetch(`${this.apiUrl}/login`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ login, password })
+    })
+      .then(response => response.json())
+      .then(data => {
+        if (data.token) {
+          localStorage.setItem('token', data.token);
+          this.router.navigate(['/home']);
+        } else {
+          alert('Invalid login or password');
+        }
+      })
+      .catch(() => alert('Server error'));
   }
 }
