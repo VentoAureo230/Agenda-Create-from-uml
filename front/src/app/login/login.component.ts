b4d706c21e32f5158cd461e903af7d1cb57651bf
anthonymeny <anthony.meny@noctua.care>
2024-04-30 15:43:53 +0200

    save state

--- front/src/app/login/login.component.ts
+++ front/src/app/login/login.component.ts
@@ -1,17 +1,29 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.sass'
 })
 export class LoginComponent {
-  constructor(private router: Router) { }
+  login!: string;
+  password!: string;
+  
+  constructor(private router: Router, private http: HttpClient) { }
 
-  login() {
-    this.router.navigate(['/home']);
+  loginUser(login: string, password: string) {
+    this.http.post('http://localhost:3000/login', { login: this.login, password: this.password}).subscribe((res: any) => {
+      if (res.token) {
+        localStorage.setItem('token', res.token);
+        this.router.navigate(['/home']);
+      } else {
+        alert('Incorrect login or password');
+      }
+    });
   }
 }
